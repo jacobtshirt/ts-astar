c6f161f0c5075cd9a9e782a906ed2058a6661afa
Jacob Tschirhart <jacobtshirt@gmail.com>
2016-12-01 10:12:16 -0600

    added start/goal nodes and obstacle delimiter to config, changed all Location references to 'Node', moved isStartNode out of reconstruct path, added canMoveToNode function to cut down on duplicated code

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,19 @@
-namespace Config {
+import { Node } from "./node";
+
+export namespace Config {
     let _navigation: string;
     let _width: number;
     let _height: number;
+    let _obstacleChar: string;
+    let _startNode: Node;
+    let _goalNode: Node;
 
+    /**
+     * Get/Set the navigation style (allowed directional movement)
+     * Manhattan | Diagonal | Euclidean
+     * @param newNavigation
+     * @returns {string}
+     */
     export function navigation(newNavigation?: string): string {
         if (newWidth) {
             _navigation = newNavigation;
@@ -10,6 +21,11 @@ namespace Config {
         return _navigation;
     }
 
+    /**
+     * Get/Set width of grid
+     * @param newWidth
+     * @returns {number}
+     */
     export function width(newWidth?: number): number {
         if (newWidth) {
             _width = newWidth;
@@ -17,10 +33,51 @@ namespace Config {
         return _width;
     }
 
+    /**
+     * Get/Set height of grid
+     * @param newHeight
+     * @returns {number}
+     */
     export function height(newHeight?: number): number {
       if (newHeight) {
           _height = newHeight;
       }
       return _height;
+    }
+
+    /**
+     * Get/set the character used to represent obstacles on the grid
+     * @param newObstacleChar
+     * @returns {string}
+     */
+    export function obstacleChar(newObstacleChar?: number): number {
+        if (newHeight) {
+            _obstacleChar = newObstacleChar;
+        }
+        return _obstacleChar;
+    }
+
+    /**
+     * Get/set node that the agent should start at
+     * @param newStartNode
+     * @returns {Node}
+     */
+    export function startNode(newStartNode?: Node): Node {
+        if (newStartNode) {
+            _startNode = newObstacleChar;
+        }
+        return _startNode;
+    }
+
+    /**
+     * Get/set node that the agent should end at
+     * @param newGoalNode
+     * @returns {Node}
+     */
+    export function goalNode(newGoalNode?: Node): Node {
+        if (newGoalNode) {
+            _goalNode = newGoalNode;
+        }
+        return _goalNode;
     }
 }
--- src/utility.ts
+++ src/utility.ts
@@ -1,56 +1,108 @@
-import {Location} from "./Location";
+import { Config } from "./config";
+import { Node } from "./node";
 
-function getManhattanNeighbors(grid: Map<Location, string>, x: number, y: number){
-    let width: number = Math.sqrt(grid.size);
-    let height: number = width;
-    let neighbors: Set<Location> = new Set<Location>();
-    if (x - 1 >= 0) {
-        let l = new Location(x - 1, y);
-        if (grid.get(l) !== "#") {
-            neighbors.add(l);
+function getEuclideanNeighbors() {
+
+}
+
+function getDiagonalNeighbors(grid: Map<Node, string>, x: number, y: number): Set<Node> {
+    const width: number = Config.width();
+    const height: number = Config.height();
+    let neighbors: Set<Node> = new Set<Node>(getManhattanNeighbors());
+
+    // Check northwest neighbor
+    if (x - 1 >= 0 && y + 1 < height) {
+        let node = new Node(x - 1,  y + 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
         }
     }
-    if (x + 1 < width) {
-        let l = new Location(x + 1, y);
-        if (grid.get(l) !== "#") {
-            neighbors.add(l);
+
+    // Check northeast neighbor
+    if (x + 1 < width && y + 1 < height) {
+        let node = new Node(x + 1,  y + 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
         }
     }
-    if (y - 1 >= 0) {
-        let l = new Location(x, y - 1);
-        if (grid.get(l) !== "#") {
-            neighbors.add(l);
+
+    // Check southwest neighbor
+    if (x - 1 >= 0 && y - 1 >= 0) {
+        let node = new Node(x - 1,  y - 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
         }
     }
-    if (y + 1 < height) {
-        let l = new Location(x, y + 1);
-        if (grid.get(l) !== "#") {
-            neighbors.add(l);
+
+    // Check southeast neighbor
+    if (x + 1 < width && y - 1 >= 0) {
+        let node = new Node(x + 1,  y - 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
         }
     }
 
     return neighbors;
 }
 
-function isSameLocation(lhs: Location, rhs: Location){
-    return lhs.x === rhs.x && lhs.y === rhs.y;
+function getManhattanNeighbors(grid: Map<Node, string>, x: number, y: number): Set<Node> {
+    const width: number = Config.width();
+    const height: number = Config.height();
+    let neighbors: Set<Node> = new Set<Node>();
+
+    // Check west neighbor
+    if (x - 1 >= 0) {
+        let node = new Node(x - 1, y);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
+        }
+    }
+    // Check east neighbor
+    if (x + 1 < width) {
+        let node = new Node(x + 1, y);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
+        }
+    }
+    // Check south neighbor
+    if (y - 1 >= 0) {
+        let node = new Node(x, y - 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
+        }
+    }
+    // Check north neighbor
+    if (y + 1 < height) {
+        let node = new Node(x, y + 1);
+        if (canMoveToNode(grid, node)) {
+            neighbors.add(node);
+        }
+    }
+
+    return neighbors;
 }
 
-function reconstructPath(cameFrom: Map<Location, Location>, start: Location, current: Location){
-    let path: Set<Location> = new Set<Location>();
-    function isStart(current: Location){
-        return current.x == start.x && current.y == start.y;
-    }
+function isSameNode(lhs: Node, rhs: Node): boolean {
+    return lhs.x === rhs.x
+        && lhs.y === rhs.y;
+}
+
+function reconstructPath(cameFrom: Map<Node, Node>, current: Node): Set<Node> {
+    let path: Set<Node> = new Set<Node>();
     do {
         path.add(current);
         current = cameFrom.get(current);
     }
-    while(!isStart(current));
+    while (!isStartNode(current));
 
     return path;
 }
 
-function distanceBetween(current: Location, neighbor: Location){
+function isStartNode(curr: Node): boolean {
+    return isSameNode(curr, Config.startNode());
+}
+
+function distanceBetween(current: Node, neighbor: Node){
     let distance: number;
     let cX = current.x;
     let cY = current.y;
@@ -64,5 +116,9 @@ function distanceBetween(current: Location, neighbor: Location){
     return distance;
 }
 
-export {getManhattanNeighbors as getManhattanNeighbors, isSameLocation as isSameLocation, reconstructPath as reconstructPath,
+function canMoveToNode(grid: Map<Node, string>, nodeToCheck: Node) {
+    return grid.get(nodeToCheck) !== Config.obstacleChar();
+}
+
+export {getManhattanNeighbors as getManhattanNeighbors, isSameNode as isSameNode, reconstructPath as reconstructPath,
     distanceBetween as distanceBetween};
