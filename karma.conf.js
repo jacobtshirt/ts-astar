ef2ac9608d32e8c7246ed81d046b34d092522aef
Tschirhart, Jacob L <JTSCHIRH@amfam.com>
2016-12-02 07:56:04 -0600

    init karma testing suite

---
+++ karma.conf.js
@@ -0,0 +1,86 @@
+// Karma configuration
+// Generated on Fri Dec 02 2016 07:46:52 GMT-0600 (CST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'karma-typescript'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      { pattern: "src/**/*.spec.ts" }
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+       "src/**/*.ts": ["karma-typescript"]
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'karma-typescript'],
+
+    karmaTypescriptConfig: {
+        tsconfig: './tsconfig.json',
+        reports:
+        {
+            "html": "coverage",
+            "teamcity": "coverage",
+        },
+        remapOptions:
+        {
+            // Regex or string for excluding files, the example below is default
+            exclude: /(typings|Interfaces|Libs)/,
+            // Function for warning messages, these warnings are silent by default
+            warn: function (message) { }
+        }
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: 1
+  })
+}
