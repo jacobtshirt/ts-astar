6bb3c7cf255f05f0e8aeb6bede59d097c589a1cc
Tschirhart, Jacob L <JTSCHIRH@amfam.com>
2016-12-07 13:44:46 -0600

    fix manhattan heuristic

--- src/heuristics.ts
+++ src/heuristics.ts
@@ -1,6 +1,7 @@
 import { Node } from "./node";
 
-export function getManhattanHeuristic(curr: Node, neighbor: Node): number {
+export function getManhattanHeuristic<T extends Node>(curr: T, neighbor: T): number {
     const dx = Math.abs(curr.x - neighbor.x);
-    return dx;
+    const dy = Math.abs(curr.y - neighbor.y);
+    return dx + dy;
 }
