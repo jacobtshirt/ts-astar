f4e61024e25d2247492db32c8ff83c58f737fe31
Tschirhart, Jacob L <JTSCHIRH@amfam.com>
2016-12-22 10:44:58 -0600

    Configuration object type and refactoring

--- src/a-star.ts
+++ src/a-star.ts
@@ -1,4 +1,4 @@
-import { Config } from "./config";
+import { ConfigController } from "./config";
 import { EdgeList } from "./custom-types";
 import { Edge } from "./edge";
 import { getManhattanHeuristic } from "./heuristics"
@@ -7,45 +7,66 @@ import { isSameNode, reconstructPath, reconstructEdgePath, getDiagonalNeighbors,
 import { OrderedMap, OrderedSet, Map, Set } from 'immutable';
 import * as _ from 'lodash';
 
+type Configuration<T extends Node> = {
+    navigation?: string
+    , detectObstacle: Function
+    , startNode: T
+    , goalNode: T
+}
 
-export function astar<T extends Node, V>(start: T, goal: T, grid?: Map<T, V>, navigation: string = 'Manhattan') {
+
+export function astar<T extends Node, V>(grid: Map<T, V>, config: Configuration<T>) {
     let path: Array<T> = new Array<T>();
 
-    Config.navigation(navigation);
-    Config.goalNode(goal);
-    Config.startNode(start);
+    ConfigController.navigation(config.navigation);
+    ConfigController.obstacleDetectionFn(config.detectObstacle);
+    ConfigController.startNode(config.startNode);
+    ConfigController.goalNode(config.goalNode);
 
     if(grid instanceof Map && !(grid instanceof Array)) {
-        path = searchGrid<T, V>(start, goal, grid);
+        path = searchGrid<T, V>(grid);
     }
     return path;
 }
 
-function searchGrid<T extends Node, V>(start: T, goal: T, grid: Map<T, V>): Array<T> {
+function searchGrid<T extends Node, V>(grid: Map<T, V>): Array<T> {
     let path: Array<T>;
     let cameFrom: Map<T, T> = Map<T, T>();
-
-    const getNeighbors = Config.navigation() === 'Diagonal'
-                            ? getDiagonalNeighbors(grid) 
-                            : getManhattanNeighbors(grid);
+    let getNeighborsFn: Function;
+    let nav = ConfigController.navigation();
+    
 
     let openSet: OrderedMap<T, number> = OrderedMap<T, number>();
     let closedSet:  Set<T> =  Set<T>();
     let costSoFar: Map<T, number> = Map<T, number>();
+    let start = ConfigController.startNode();
     openSet = openSet.set(start, 0);
     costSoFar = costSoFar.set(start, 0);
+
+    switch(nav) {
+        case 'DIAGONAL':
+            getNeighborsFn = getDiagonalNeighbors(grid);
+            break;
+        case 'EUCLIDIAN':
+            getNeighborsFn = getEuclideanNeighbors(grid);
+            break;
+        case 'MANHATTAN':
+        default:
+            getNeighborsFn = getManhattanNeighbors(grid);
+            break;
+    }
 
     while(!openSet.isEmpty()) {
         const currentPriority = openSet.min();
         const currentNode = openSet.keyOf(currentPriority);
         openSet = openSet.delete(currentNode);
         closedSet = closedSet.add(currentNode);
-        if(_.isEqual(currentNode, goal)){
+        if(_.isEqual(currentNode, ConfigController.goalNode())){
             path = reconstructPath<T>(cameFrom, currentNode);
             break;
         }
 
-        const neighbors = getNeighbors(currentNode.x, currentNode.y);
+        const neighbors = getNeighborsFn(currentNode.x, currentNode.y);
        
         for(let neighbor of neighbors) {
             
