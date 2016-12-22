5763cdb4843dfced2279000f50973ddc2efee1d0
Tschirhart, Jacob L <JTSCHIRH@amfam.com>
2016-12-22 11:13:49 -0600

    added comments a* search

--- src/a-star.ts
+++ src/a-star.ts
@@ -16,14 +16,20 @@ type Configuration<T extends Node> = {
 
 
 export function astar<T extends Node, V>(searchableGrid: Map<T, V> | Immutable.Map<T, V>, config: Configuration<T>) {
+    // Define and initialize path returned by astar.
+    // Empty path is returned if no path is found
+    // A* always finds a path.
     let path: Array<T> = new Array<T>();
+    // Make sure grid is an Immutable Map, not an ES6 object.
     const grid: Immutable.Map<T, V> = Immutable.Map<T,V>(searchableGrid);
 
+    // Configuration
     ConfigController.navigation(config.navigation);
     ConfigController.obstacleDetectionFn(config.detectObstacle);
     ConfigController.startNode(config.startNode);
     ConfigController.goalNode(config.goalNode);
 
+    // Search
     if(grid instanceof Map && !(grid instanceof Array)) {
         path = searchGrid<T, V>(grid);
     }
@@ -31,19 +37,23 @@ export function astar<T extends Node, V>(searchableGrid: Map<T, V> | Immutable.M
 }
 
 function searchGrid<T extends Node, V>(grid: Immutable.Map<T, V>): Array<T> {
-    let path: Array<T>;
-    let cameFrom: Immutable.Map<T, T> = Immutable.Map<T, T>();
-    let getNeighborsFn: Function;
-    let nav = ConfigController.navigation();
+    // Get starting node off config
+    const start = ConfigController.startNode();
     
+    // Define path that evenutally gets returned by searchGrid.
+    // standard ES6 Array.
+    let path: Array<T>;
 
+    // Initialize map/grid travel data stores.
+    // Immutable
+    let cameFrom: Immutable.Map<T, T> = Immutable.Map<T, T>();
     let openSet: Immutable.OrderedMap<T, number> = Immutable.OrderedMap<T, number>();
     let closedSet: Immutable.Set<T> = Immutable.Set<T>();
     let costSoFar: Immutable.Map<T, number> = Immutable.Map<T, number>();
-    let start = ConfigController.startNode();
-    openSet = openSet.set(start, 0);
-    costSoFar = costSoFar.set(start, 0);
-
+    
+    // Initialize navigation and neighbor checking
+    let getNeighborsFn: Function;
+    const nav = ConfigController.navigation();
     switch(nav) {
         case 'DIAGONAL':
             getNeighborsFn = getDiagonalNeighbors(grid);
@@ -57,34 +67,48 @@ function searchGrid<T extends Node, V>(grid: Immutable.Map<T, V>): Array<T> {
             break;
     }
 
+    // Insert starting node into the open (frontier) and [travel] cost sets.
+    openSet = openSet.set(start, 0);
+    costSoFar = costSoFar.set(start, 0);
+
     while(!openSet.isEmpty()) {
+        // Pull next node off openSet based on lowest priority (lowest heuristic)
         const currentPriority = openSet.min();
         const currentNode = openSet.keyOf(currentPriority);
+        // Remove from the open set and add to closed (visited) set 
         openSet = openSet.delete(currentNode);
         closedSet = closedSet.add(currentNode);
+
+        // Goal check
         if(_.isEqual(currentNode, ConfigController.goalNode())){
             path = reconstructPath<T>(cameFrom, currentNode);
             break;
         }
 
+        // Collect neighbors of current node
         const neighbors = getNeighborsFn(currentNode.x, currentNode.y);
        
+        // For each neighbor
         for(let neighbor of neighbors) {
             
+            // If neighbor has been visited, skip to next neighbor
             if(closedSet.has(neighbor)) continue;
     
+            // add the cost to move to the neighbor to the current travel cost
             const tempCost = costSoFar.get(currentNode) + distanceBetween(currentNode, neighbor);
+            // if not in costSoFar or the new cost is less than what we currently know about cost to travel to the neighbor...
             if(!costSoFar.has(neighbor) || tempCost < costSoFar.get(neighbor)) {
-                const priority = tempCost + getManhattanHeuristic(currentNode, neighbor);
+                // Calcuate priority (heuristic + travel cost);
+                const priority = tempCost + getManhattanHeuristic(currentNode, neighbor); // TODO: fix for diagonal and euclidean
+
+                // Set the new, lower travel cost of the neighbor
                 costSoFar = costSoFar.set(neighbor, tempCost);
+                // Add neighbor to the open set
                 openSet = openSet.set(neighbor, priority);
+                // breadcrumbs for finding our away back to the start
                 cameFrom = cameFrom.set(neighbor, currentNode);
             }
         }
-    
-        
     }
-
     return path;
-
 }
